01daa44cf9f4485c43984564b9b55e69040266fd
joseluiz <joseluiz@fedora>
2022-11-08 21:47:41 -0300

    criação da api para criar as pastas e arquivos de log

---
+++ src/com/api/main/ApiMain.java
@@ -0,0 +1,16 @@
+package com.api.main;
+
+/**
+ *
+ * @author joseluiz
+ */
+public class ApiMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
